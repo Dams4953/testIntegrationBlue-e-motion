a5dadc23ba5a2cb0856edc194a9c655c24b76c17
dams <legatdeus@gmail.com>
2024-05-21 16:31:47 +0200

    chemins relatifs

--- js/carouselPictures.js
+++ js/carouselPictures.js
@@ -1,66 +1,66 @@
 const images = [
     { 
-        src: "assets/pictures/ISUZU-D-MAX-Geschaeftskunden-Flottenkunden.webp", 
+        src: "./assets/pictures/ISUZU-D-MAX-Geschaeftskunden-Flottenkunden.webp", 
         alt: "ISUZU-D-MAX-Geschaeftskunden-Flottenkunden" 
     },
     { 
-        src: "assets/pictures/ISUZU-D-MAX-Individualisierung-Umbauten.webp", 
+        src: "./assets/pictures/ISUZU-D-MAX-Individualisierung-Umbauten.webp", 
         alt: "ISUZU-D-MAX-Individualisierung-Umbauten" 
     },
     { 
-        src: "assets/pictures/ISUZU-D-MAX-blau-im-wald-nachhaltige-effizienz.webp", 
+        src: "./assets/pictures/ISUZU-D-MAX-blau-im-wald-nachhaltige-effizienz.webp", 
         alt: "ISUZU-D-MAX-blau-im-wald-nachhaltige-effizienz" 
     },
     { 
-        src: "assets/pictures/04-home-bild_text-N-Serie 3,5t Koffer City Image.webp", 
+        src: "./assets/pictures/04-home-bild_text-N-Serie 3,5t Koffer City Image.webp", 
         alt: "04-home-bild_text-N-Serie 3,5t Koffer City Image" 
     },
     { 
-        src: "assets/pictures/isuzu-d-max-Ladefläche-Handwerker-Kipper.webp", 
+        src: "./assets/pictures/isuzu-d-max-Ladefläche-Handwerker-Kipper.webp", 
         alt: "isuzu-d-max-Ladefläche-Handwerker-Kipper" 
     },
     { 
-        src: "assets/pictures/isuzu-d-max-zubehoer.webp", 
+        src: "./assets/pictures/isuzu-d-max-zubehoer.webp", 
         alt: "isuzu-d-max-zubehoer" 
     },
     { 
-        src: "assets/pictures/02-D_MAX-image_text_outbreak-20MY ISUZU D-MAX_5_EU_LHD_Crew RBD(Chassis Overlay) wihout Trailer&Talents.webp", 
+        src: "./assets/pictures/02-D_MAX-image_text_outbreak-20MY ISUZU D-MAX_5_EU_LHD_Crew RBD(Chassis Overlay) wihout Trailer&Talents.webp", 
         alt: "isuzi-d-max-image_text_outbreak-20MY ISUZU D-MAX_5_EU_LHD_Crew RBD(Chassis Overlay) wihout Trailer&Talents" 
     },
     { 
-        src: "assets/pictures/ISUZU-D-MAX-blue-driving-in-water.webp", 
+        src: "./assets/pictures/ISUZU-D-MAX-blue-driving-in-water.webp", 
         alt: "isuzi-d-max-blue-driving-in-water" 
     },
     { 
-        src: "assets/pictures/02-isuzu-NSerie-carousel-BIG-N-Serie Kofferaufbau 1~-~media--d98c6261--query.webp", 
+        src: "./assets/pictures/02-isuzu-NSerie-carousel-BIG-N-Serie Kofferaufbau 1~-~media--d98c6261--query.webp", 
         alt: "izusu-NSerie-carousel-BIG-N-Serie Kofferaufbau 1" 
     },
     { 
-        src: "assets/pictures/isuzu-d-max-ueber-uns.webp", 
+        src: "./assets/pictures/isuzu-d-max-ueber-uns.webp", 
         alt: "isuzu-d-max-ueber-uns" 
     },
     { 
-        src: "assets/pictures/isuzu-d-max-zubehoer.webp", 
+        src: "./assets/pictures/isuzu-d-max-zubehoer.webp", 
         alt: "isuzu-d-max-zubehoer" 
     },
     { 
-        src: "assets/pictures/isuzu-N-Serie-carousel-BIG-N-Serie Absetzkipper~-~media--d98c6261--query.webp", 
+        src: "./assets/pictures/isuzu-N-Serie-carousel-BIG-N-Serie Absetzkipper~-~media--d98c6261--query.webp", 
         alt: "isuzi-N-Serie-carousel-BIG-N-Serie Absetzkipper" 
     },
     { 
-        src: "assets/pictures/ISUZU-D-MAX-blue-driving-in-water.webp", 
+        src: "./assets/pictures/ISUZU-D-MAX-blue-driving-in-water.webp", 
         alt: "isuzi-d-max-blue-driving-in-water" 
     },
     { 
-        src: "assets/pictures/isuzu-N-Serie-carousel-BIG-N-Serie-3,5t Pritsche~-~media--d98c6261--query.webp", 
+        src: "./assets/pictures/isuzu-N-Serie-carousel-BIG-N-Serie-3,5t Pritsche~-~media--d98c6261--query.webp", 
         alt: "isuzu-N-Serie-carousel-BIG-N-Serie-3,5t Pritsche" 
     },
     { 
-        src: "assets/pictures/ISUZU-D-MAX-blau-im-wald-nachhaltige-effizienz.webp", 
+        src: "./assets/pictures/ISUZU-D-MAX-blau-im-wald-nachhaltige-effizienz.webp", 
         alt: "isizu-d-max-blau-im-wald-nachhaltige-effizienz" 
     },
     { 
-        src: "assets/pictures/isuzu-d-max-ueber-uns.webp", 
+        src: "./assets/pictures/isuzu-d-max-ueber-uns.webp", 
         alt: "isuzu-d-max-ueber-uns" 
     }
 ];
